db47241034e28c6af6dffb2c3afe363cc5e23365
Duyet Nguyen <duyetlnguyen@gmail.com>
2017-02-01 13:07:07 -0800

    Add assign_type

--- TypeInferencer.v
+++ TypeInferencer.v
@@ -1,34 +1,31 @@
 Require Import String.
 Require Import List.
+Require Import Nat.
 
 Open Scope string_scope.
 Open Scope list_scope.
+Open Scope core_scope.
 
 Import ListNotations.
 
 Require Import AST.
 Require Import Util.
 
-Definition environment := list (string * type) % type.
-Definition substitution := list (nat * type) % type.
-Definition constraint := list (nat * type) % type.
+Definition environment := (@dict string (type * bool)) % type. (* bool indicates if it is let-bound variable *)
+Definition substitution := (@dict nat type) % type.
+Definition constraint := (@dict nat type) % type.
 
 Inductive typed_expr : Type :=
   | TpNum : nat -> type -> typed_expr
   | TpBool : bool -> type -> typed_expr
   | TpVar : string-> type -> typed_expr
   | TpIf : typed_expr -> typed_expr -> typed_expr -> type -> typed_expr
-  | TpNFun : typed_expr -> type -> typed_expr
-  | TpUFun : string -> option type -> typed_expr -> type -> typed_expr
+  | TpFun : string -> type -> typed_expr -> type -> typed_expr
   | TpCall : typed_expr -> typed_expr -> type -> typed_expr
-  | TpLet : string -> typed_expr -> typed_expr -> type -> typed_expr
+  | TpLet : string -> type -> typed_expr -> typed_expr -> type -> typed_expr
   | TpBinop : binop -> typed_expr -> typed_expr -> type -> typed_expr
-(*| TpDatum : string -> list typed_expr -> type -> typed_expr
-  | TpMatch : typed_expr -> list pattern -> list typed_expr -> type -> typed_expr *)
   | TpCons : typed_expr -> typed_expr -> type -> typed_expr
-  | TpNil : type -> typed_expr
-  | TpDelay : typed_expr -> type -> typed_expr
-  | TpForce : typed_expr -> type -> typed_expr.
+  | TpNil : type -> typed_expr.
 
 Definition type_from_typed_expr  (texpr : typed_expr) : type :=
   match texpr with
@@ -36,83 +33,124 @@ Definition type_from_typed_expr  (texpr : typed_expr) : type :=
     | TpBool _ t => t
     | TpVar _ t => t
     | TpIf _ _ _ t => t
-    | TpNFun _ t => t
-    | TpUFun _ _ _ t => t
+    | TpFun _ _ _ t => t
     | TpCall _ _ t => t
-    | TpLet _ _ _ t => t
+    | TpLet _ _ _ _ t => t
     | TpBinop _ _ _ t => t
-(*  | TpDatum _ _ t => t
-    | TpMatch _ _ _ t => t *)
     | TpCons _ _ t => t
     | TpNil t => t
-    | TpDelay _ t => t
-    | TpForce _ t => t
   end.
 
-Fixpoint type_from_env (x : string) (env : environment) : option type :=
-  match env with
-    | [] => None
-    | (v, t)::l => if str_eqb x v then Some t else type_from_env x l
-  end.
+Definition type_from_env (x : string) (env : environment) : option ((type * bool) % type) :=
+  dict_get x str_eqb env.
 
-
-(* Need environment for TmLet and TmUFun *)
-(* Need level for let-polymorphism *)
-Fixpoint assign_type (ex : expr) (fv : nat)  : (nat * typed_expr) % type :=
-  match ex with
-    | TmNum n => pair fv (TpNum n TNum)
-    | TmBool b => pair fv (TpBool b TBool)
-    | TmVar x => pair (fv + 1) (TpVar x (TVar fv))
-    | TmIf c e1 e2 =>
-      let (c_fv, c_tex) := assign_type c fv in
-      let (e1_fv, e1_tex) := assign_type e1 c_fv in
-      let (e2_fv, e2_tex) := assign_type e2 e1_fv in
-        pair (e2_fv + 1) (TpIf c_tex e1_tex e2_tex (TVar e2_fv))
-    | TmNFun e =>
-      let (e_fv, e_tex) := assign_type e fv in
-        pair (e_fv + 1) (TpNFun e_tex (TFun TUnit (TVar e_fv)))
-    | TmUFun x tx e =>
-      let (e_fv, e_tex) := assign_type e fv in
-      match tx with
-        | Some t => pair (e_fv + 1) (TpUFun x tx e_tex (TFun t (TVar e_fv)))
-        | None => pair (e_fv + 2) (TpUFun x tx e_tex (TFun (TVar e_fv) (TVar (e_fv + 1))))
+Fixpoint copy_type (t : type) (fv : nat) (d : @dict nat nat): (nat * type * (@dict nat nat)) % type :=
+  match t with
+    | TNum => (fv, t, d)
+    | TBool => (fv, t, d)
+    | TFun x e =>
+      let '(x_fv, x_t, d1) := copy_type x fv d in
+      let '(e_fv, e_t, d2) := copy_type e x_fv d1 in
+        (e_fv, (TFun x_t e_t), d2)
+    | TList l =>
+      let '(l_fv, l_t, d1) := copy_type l fv d in
+        (l_fv, (TList l_t), d1)
+    | TVar x =>
+      match dict_get x eqb d with
+        | None => ((fv + 1), (TVar fv), (x, fv)::d)
+        | Some v => (fv, (TVar v), d)
       end
-    | TmCall f x =>
-      let (f_fv, f_tex) := assign_type f fv in
-      let (x_fv, x_tex) := assign_type x f_fv in
-        pair (x_fv + 1) (TpCall (f_tex) (x_tex) (TVar (x_fv)))
-    | TmLet x e1 e2 =>
-      let (e1_fv, e1_tex) := assign_type e1 fv in
-      let (e2_fv, e2_tex) := assign_type e2 e1_fv in
-        pair (e2_fv + 1) (TpLet x e1_tex e2_tex (TVar (e2_fv)))
-    | TmBinop op e1 e2 =>
-      let (e1_fv, e1_tex) := assign_type e1 fv in
-      let (e2_fv, e2_tex) := assign_type e2 e1_fv in
-        pair (e2_fv + 1) (TpBinop op e1_tex e2_tex (TVar e2_fv))
-(*  | TmDatum d l =>
-      match l with
-        | [] => pair fv (TpDatum d [] (TDatum d []))
-        | _::_ =>
-          (* l_ex and p_ex are lists of typed_expr *)
-          let (l_fv, l_ex) := fold_left (fun (p : (nat * list typed_expr) % type) (e : expr) =>
-            let (p_fv, p_ex) := p in
-            let (e_fv, e_ex) := assign_type e p_fv in
-              pair e_fv (p_ex ++ [e_ex])) l (pair fv []) in
+    | TUnit => (fv, t, d)
+  end.
 
-          let (tv_fv, tv_l) := fold_left (fun (p : (nat * list type) % type) (e : expr) =>
-            let (p_fv, p_l) := p in
-              pair (p_fv + 1) (p_l ++ [TVar p_fv])) l (pair l_fv []) in
-          pair tv_fv (TpDatum d l_ex (TDatum d tv_l))
-      end *)
+Fixpoint assign_type (ex : expr) (fv : nat) (env : environment) : option (nat * typed_expr) % type :=
+   match ex with
+    (* TmNum *)
+    | TmNum n => Some (fv, (TpNum n TNum))
+
+    (* TmBool *)
+    | TmBool b => Some (fv, (TpBool b TBool))
+
+    (* TmVar *) (* must copy *)
+    | TmVar x =>
+      match type_from_env x env with
+      | Some (t, b) => Some (fv, TpVar x t)
+      | None => None
+      end
+
+    (* TmIf *)
+    | TmIf c e1 e2 =>
+      match assign_type c fv env with
+        | Some (c_fv, c_tex) =>
+          match assign_type e1 c_fv env with
+            | Some (e1_fv, e1_tex) =>
+              match assign_type e2 e1_fv env with
+                | Some (e2_fv, e2_tex) => Some (e2_fv + 1, (TpIf c_tex e1_tex e2_tex (TVar e2_fv)))
+                | _ => None
+              end
+            | _ => None
+          end
+        | _ => None
+      end
+
+    (* TmFun *)
+    | TmFun x tx e =>
+      let (x_tv, x_fv) :=
+        match tx with
+          | Some t => (t, fv)
+          | None => (TVar fv, fv + 1)
+        end
+      in
+        match assign_type e x_fv ((x, (x_tv, false))::env) with
+          | Some (e_fv, e_tex) => Some (e_fv + 1, TpFun x x_tv e_tex (TFun x_tv (TVar e_fv)))
+          | None => None
+        end
+
+    (* TmCall *)
+    | TmCall f x =>
+      match assign_type f fv env with
+        | Some (f_fv, f_tex) =>
+          match assign_type x f_fv env with
+            | Some (x_fv, x_tex) => Some (x_fv + 1, (TpCall f_tex x_tex (TVar x_fv)))
+            | None => None
+          end
+        | None => None
+      end
+
+    (* TmLet *) (* support recursion ? *)
+    | TmLet x e1 e2 =>
+      let (x_tv, x_fv) := (TVar fv, fv + 1) in
+        match assign_type e1 x_fv env with
+          | Some (e1_fv, e1_tex) =>
+            match assign_type e2 e1_fv ((x, (x_tv, true))::env) with
+              | Some (e2_fv, e2_tex) => Some (e2_fv + 1, (TpLet x x_tv e1_tex e2_tex (TVar e2_fv)))
+              | None => None
+            end
+          | None => None
+        end
+
+    (* TmBinop *)
+    | TmBinop op e1 e2 =>
+      match assign_type e1 fv env with
+        | Some (e1_fv, e1_tex) =>
+          match assign_type e2 e1_fv env with
+            | Some (e2_fv, e2_tex) => Some (e2_fv + 1, (TpBinop op e1_tex e2_tex (TVar e2_fv)))
+            | None => None
+          end
+        | None => None
+      end
+
+    (* TmCons *)
     | TmCons h t =>
-      let (h_fv, h_tex) := assign_type h fv in
-      let (t_fv, t_tex) := assign_type t h_fv in
-        pair (t_fv + 1) (TpCons h_tex t_tex (TList (TVar t_fv)))
-    | TmNil => pair (fv + 1) (TpNil (TList (TVar fv)))
-    | TmForce e =>
-      let (e_fv, e_tex) := assign_type e fv in
-        pair (e_fv + 1) (TpForce e_tex (TVar e_fv))
-    | TmDelay e =>
-      let (e_fv, e_tex) := assign_type e fv in
-        pair (e_fv + 1) (TpDelay e_tex (TVar e_fv))
+      match assign_type h fv env with
+        | Some (h_fv, h_tex) =>
+          match assign_type t h_fv env with
+            | Some (t_fv, t_tex) => Some (t_fv + 1, (TpCons h_tex t_tex (TList (TVar t_fv))))
+            | None => None
+          end
+        | None => None
+      end
+
+    (* TmNil *)
+    | TmNil => Some (fv + 1, (TpNil (TList (TVar fv))))
   end.
