0390f3ab9dbe10d58656b768d4e9892373cd6c12
Duyet Nguyen <duyetlnguyen@gmail.com>
2017-02-07 20:30:25 -0800

    Use partial maps

--- AST.v
+++ AST.v
@@ -6,7 +6,7 @@ Open Scope list_scope.
 
 Import ListNotations.
 
-Require Import Util.
+Require Import Maps.
 
 Inductive type : Type :=
   | TNum : type
@@ -31,9 +31,9 @@ Inductive binop : Type :=
 Inductive ut_expr : Type := (* ut_expr : untyped expression *)
   | ut_Num : nat -> ut_expr
   | ut_Bool : bool -> ut_expr
-  | ut_Var : string -> ut_expr
+  | ut_Var : id -> ut_expr
   | ut_If : ut_expr -> ut_expr -> ut_expr -> ut_expr
-  | ut_Fun : string -> ut_expr -> ut_expr
+  | ut_Fun : id -> ut_expr -> ut_expr
   | ut_Call : ut_expr -> ut_expr -> ut_expr
   | ut_Binop : binop -> ut_expr -> ut_expr -> ut_expr
   | ut_Cons : ut_expr -> ut_expr -> ut_expr
@@ -42,15 +42,13 @@ Inductive ut_expr : Type := (* ut_expr : untyped expression *)
 Inductive t_expr : Type := (* t_expr : typed expression *)
   | t_Num : nat -> t_expr
   | t_Bool : bool -> t_expr
-  | t_Var : string -> t_expr
+  | t_Var : id -> t_expr
   | t_If : t_expr -> t_expr -> t_expr -> t_expr
-  | t_Fun : string -> type -> t_expr -> t_expr
+  | t_Fun : id -> type -> t_expr -> t_expr
   | t_Call : t_expr -> t_expr -> t_expr
   | t_Binop : binop -> t_expr -> t_expr -> t_expr
   | t_Cons : t_expr -> t_expr -> t_expr
   | t_Nil : type -> t_expr.
 
-Definition environment := (@dict string type) % type.
-
-Definition type_from_env (x : string) (env : environment) : option type :=
-  dict_get x str_eqb env.
+Definition environment := partial_map type.
+Definition constraint := list (type * type) % type.
